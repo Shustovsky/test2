e48451a70e9702461ce625499239805e889f08ae
A. Shustovsky <a.shustovsky@gmail.com>
2022-06-22 11:42:04 +0400

    ok

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const slider = tns({
+    container: '.my__slider',
+    mode: 'carousel',
+    items: 1.5,
+    gutter: 60,
+    autoWidth: true,
+    center: true,
+    controls: false,
+    mouseDrag: true,
+
+    // autoplay: true,
+    speed: 700,
+    autoplayButtonOutput: false,
+
+
+
+
+    nav: true,
+    navPosition: 'bottom',
+    navContainer: '.my__slider_btn',
+
+
+    // items: 1,
+    // slideBy: 'page',
+    // autoplay: true
+});
